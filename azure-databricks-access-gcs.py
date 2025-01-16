059da2e6ac30baf7bf0a706beb8a4dc0f332cf99
amitwangdu <38863335+amitwangdu@users.noreply.github.com>
2025-01-15 21:03:07 -0500

    Update azure-databricks-access-gcs.py

--- azure-databricks-access-gcs.py
+++ azure-databricks-access-gcs.py
@@ -5,7 +5,7 @@ pip install google-cloud-storage
 
 #step-2 
 copy the jason file to the local directory 
-here its /Workspace/Users/samit.rkl@gmail.com/Databricks-Certified-Data-Engineer-Associate/1- Databricks Lakehouse Platform/<jasonfile>.json
+here its /Workspace/Users/samit.rkl@gmail.com/Databricks-Certified-Data-Engineer-Associate/1- Databricks Lakehouse Platform/<jsonfile>.json
 
 #step-3
 
@@ -14,7 +14,7 @@ from google.oauth2 import service_account
 
 # Specify the path to the service account JSON key
 #gcs_key_path = "/dbfs/mnt/keys/skyeyez.json"  # DBFS path to the key
-gcs_key_path = "/Workspace/Users/samit.rkl@gmail.com/Databricks-Certified-Data-Engineer-Associate/1- Databricks Lakehouse Platform/<jasonfile>.json" 
+gcs_key_path = "/Workspace/Users/samit.rkl@gmail.com/Databricks-Certified-Data-Engineer-Associate/1- Databricks Lakehouse Platform/<jsonfile>.json" 
 
 # Authenticate using the service account key
 credentials = service_account.Credentials.from_service_account_file(gcs_key_path)
